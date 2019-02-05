a04a5079d723bf569cc24ffebc9f9bf0af83e945
condor <condor1226@gmail.com>
2019-02-06 08:20:04 +0900

    Altsv.dump is now implemented.

--- src/lib.rs
+++ src/lib.rs
@@ -20,17 +20,15 @@ impl HashMapWrapper {
     }
 
     fn insert(&mut self, key: &String, value: &str) {
-        unsafe {
-            let value_to_insert: Option<CString>;
-            if value.is_empty() {
-                value_to_insert = None;
-            } else {
-                value_to_insert = Some(CString::new(value).expect("This must success since it is from String"))
-            }
-            let key_to_insert = Symbol::from_string(key.clone());
-
-            self.map.insert(key_to_insert, value_to_insert);
+        let value_to_insert: Option<CString>;
+        if value.is_empty() {
+            value_to_insert = None;
+        } else {
+            value_to_insert = Some(CString::new(value).expect("This must success since it is from String"))
         }
+        let key_to_insert = Symbol::from_string(key.clone());
+
+        self.map.insert(key_to_insert, value_to_insert);
     }
 
     fn inner(self) -> HashMap<Symbol, Option<CString>> {
@@ -157,6 +155,183 @@ fn parse_line(input: &str) -> HashMapWrapper {
     line
 }
 
+const METHOD_NAME_RESPOND_TO: &[u8] = b"respond_to?\0`";
+const METHOD_NAME_TO_H: &[u8] = b"to_h\0";
+const METHOD_NAME_TO_HASH: &[u8] = b"to_hash\0";
+const METHOD_NAME_TO_S: &[u8] = b"to_s\0";
+
+const CLASS_NAME_ARGUMENT_ERROR: &str = "ArgumentError";
+
+const MESSAGE_DUMP_ERROR: &'static str = "Argument does not respond to neither :to_h nor :to_hash";
+
+unsafe fn rb_intern_u8(method_name: &[u8]) -> ID {
+    use std::mem;
+    rb_intern(mem::transmute::<&u8, c_string>(&method_name[0]))
+}
+
+unsafe fn respond_to(object: VALUE, name: ID) -> bool {
+    let respond_to = rb_intern_u8(METHOD_NAME_RESPOND_TO);
+    let method = rb_id2sym(name);
+
+    rb_funcall(object, respond_to, 1, method) == Qtrue
+}
+
+extern "C" fn join_hash(key: VALUE, value: VALUE, farg: *mut void) -> st_retval {
+    use std::mem;
+
+    let s = unsafe {mem::transmute::<*mut void, *mut DumpResult>(farg) };
+
+    match unsafe{ (*s).push_value(key, true) } {
+        None => {}
+        Some(error) => {
+            unsafe{ (*s).set_error(error) };
+            return st_retval::ST_STOP;
+        }
+    }
+
+    unsafe { (*s).push_char(':') };
+
+    match unsafe{ (*s).push_value(value, false) } {
+        None => {}
+        Some(error) => {
+            unsafe{ (*s).set_error(error) };
+            return st_retval::ST_STOP;
+        }
+    }
+    unsafe { (*s).push_char('\t') };
+
+    st_retval::ST_CONTINUE
+}
+
+struct DumpResult {
+    error: Option<Error>,
+    buffer: String,
+}
+
+impl DumpResult {
+    fn new(item_count: usize) -> Self{
+        DumpResult {
+            buffer: String::with_capacity(item_count * (BUFFER_SIZE_KEY + BUFFER_SIZE_VALUE)),
+            error: None,
+        }
+    }
+    fn push_value(&mut self, v: VALUE, for_key: bool) -> Option<Error>{
+        match unsafe {
+            if RB_TYPE_P(v, T_SYMBOL) {
+                String::from_ruby(rb_id2str(rb_sym2id(v)))
+            } else if RB_TYPE_P(v, T_STRING) {
+                String::from_ruby(v)
+            } else {
+                let to_s = rb_intern_u8(&METHOD_NAME_TO_S);
+                String::from_ruby(rb_funcall(v, to_s, 0))
+            }
+        } {
+            Ok(checked) => {
+                use helix::Error;
+
+                for c in String::from_checked(checked).chars() {
+                    match c {
+                        '\r' => self.buffer.push_str("\\r"),
+                        '\n' => self.buffer.push_str("\\n"),
+                        '\t' => self.buffer.push_str("\\t"),
+                        ':' => {
+                            if for_key {
+                                return Some(Error::with_c_string(CString::new("Key should not include ':'").unwrap().as_ptr()));
+                            }
+                            self.buffer.push(c)
+                        },
+                        _ => self.buffer.push(c),
+                    }
+                }
+                None
+            },
+            Err(e) => Some(e),
+        }
+    }
+
+    fn push_char(&mut self, c: char) {
+        self.buffer.push(c);
+    }
+
+    fn set_error(&mut self, e: Error) {
+        self.error = Some(e);
+    }
+
+    fn extract(self) -> Result<String, Error> {
+        match self.error {
+            Some(e) => Err(e),
+            None => Ok(String::from(self.buffer.trim_end())),
+        }
+    }
+}
+
+struct DumpArgument {
+    inner: VALUE,
+    count: usize,
+}
+
+impl DumpArgument {
+    pub unsafe fn from_hash(inner: VALUE) -> Self{
+        let count = RHASH_SIZE(inner) as usize;
+
+        DumpArgument{
+            inner,
+            count,
+        }
+    }
+    pub fn dump(&self) -> Result<String, Error> {
+        use std::mem;
+
+        let mut dump_result = DumpResult::new(self.count);
+        unsafe{
+            rb_hash_foreach(
+                self.inner,
+                join_hash,
+                mem::transmute::<&mut DumpResult, *mut void>(&mut dump_result)
+            )
+        };
+
+        dump_result.extract()
+    }
+}
+
+impl FromRuby for DumpArgument {
+    type Checked = CheckedValue<DumpArgument>;
+
+    fn from_ruby(value: VALUE) -> CheckResult<CheckedValue<DumpArgument>> {
+        unsafe {
+            if RB_TYPE_P(value, T_HASH) {
+                return Ok(CheckedValue::new(value));
+            }
+
+            let hashed_value: VALUE;
+            let to_h = rb_intern_u8(METHOD_NAME_TO_H);
+            if respond_to(value, to_h) {
+                hashed_value = rb_funcall(value, to_h, 0);
+                return Ok(CheckedValue::new(hashed_value));
+            }
+
+            let hashed_value: VALUE;
+            let to_h = rb_intern_u8(METHOD_NAME_TO_HASH);
+            if respond_to(value, to_h) {
+                hashed_value = rb_funcall(value, to_h, 0);
+                return Ok(CheckedValue::new(hashed_value));
+            }
+        }
+
+        let argument_error = unsafe {
+            let id_argument_error = rb_intern(CString::new(CLASS_NAME_ARGUMENT_ERROR).unwrap().as_ptr());
+            rb_const_get(rb_cObject, id_argument_error)
+        };
+        let argument_error_class = unsafe{ Class::from_value(argument_error) };
+        raise!(argument_error_class, MESSAGE_DUMP_ERROR);
+    }
+
+    fn from_checked(checked: CheckedValue<DumpArgument>) -> DumpArgument {
+        unsafe{ DumpArgument::from_hash(checked.to_value())}
+    }
+}
+
 ruby! {
     class Altsv {
         def parse_native(input: String) -> Vec<HashMapWrapper> {
@@ -172,6 +347,10 @@ ruby! {
         }
         def parse_line_native(input: String) -> HashMapWrapper {
             parse_line(&input)
+        }
+
+        def dump_native(value: DumpArgument) -> Result<String, Error> {
+            value.dump()
         }
     }
 }
